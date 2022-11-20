abd1b43a2a08721218ea372675258f70494491d1
Mahesh <maheshchavan0905@gmail.com>
2022-11-21 00:33:45 +0530

    inital commit

---
+++ Searchposition.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==target)
+                return i;
+            else if (nums[i]+1==target)
+                return i+1;
+            else if(nums[i]-1==target)
+               return i;
+            else if (i==nums.length-1&&nums[i]<target)
+                return i+1;
+        }
+        return 0;     
+    }
+}
