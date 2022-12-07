ce8fbefb52c37122fbefc26060dd6e54a753f7f7
Mahesh <maheshchavan0905@gmail.com>
2022-12-07 09:51:52 +0530

    Day 20

---
+++ MaximumProductSubarray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int result = nums[0];
+
+        for(int i=0;i<nums.length;i++)
+        {
+            int mul = nums[i];
+            for(int j=i+1;j<nums.length;j++)
+            {
+                result = Math.max(mul, result);
+                mul*=nums[j];
+            }
+
+            result = Math.max(mul, result);
+        }
+        return result;
+        
+    }
+}
