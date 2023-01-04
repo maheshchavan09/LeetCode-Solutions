9edae05add3021dc0f5b5449df1e8765f26ab37a
Mahesh <maheshchavan0905@gmail.com>
2023-01-04 11:50:46 +0530

    Day 37

---
+++ Find_the_Middle_Index_in_Array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int sum = 0;
+        for (int x : nums) {
+            sum += x;
+        }
+        int CurrSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (CurrSum == sum - CurrSum - nums[i])
+                return i;
+
+            CurrSum += nums[i];
+        }
+        return -1;
+
+    }
+}
